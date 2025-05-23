10fed66d40a42560b6d5d3daf9443c5aca5c683c
Meshach Nsude <nsudeme777@gmail.com>
2025-05-23 14:45:18 +0100

    project gallery completeted

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import {projectList} from "./components/DummyData";
+import PreviewModal from "./components/PreviewModal";
+import ProjectListItem from "./components/ProjectListItem";
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="h-[100vh] w-full flex items-center">
+      <Projects />
+    </div>
+  )
+}
+
+export default App;
+
+
+const Projects = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [displayModal, setDisplayModal] = useState(false);
+
+  return (
+    <section 
+      className="w-full px-desktop">
+      {/* LABELS */}
+      <div className="flex items-center justify-between mb-[10px] opacity-40">
+        <span className="w-[20%]">Project</span>
+        <div className="w-[46%] flex justify-between">
+          <span>Service</span>
+          <span>Duration</span>
+        </div>
+        <span className="w-[20%] text-right" >Year</span>
+      </div>
+
+      {/* PROJECTS */}
+      <div
+        onMouseEnter={() => setDisplayModal(true)}
+        onMouseLeave={() => setDisplayModal(false)} 
+      >
+        {
+          [...projectList, ...projectList].map((item, i) => (
+            <ProjectListItem 
+              key={`project-${i}`} 
+              project={item}
+              index={i}
+              setActiveIndex={setActiveIndex}
+            />
+          ))
+        }
+      </div>
+
+      <PreviewModal 
+        projectList={[...projectList, ...projectList]} 
+        activeIndex={activeIndex} 
+        displayModal={displayModal}/>
+    </section>
+  )
+}
---
+++ src/components/DummyData.js
@@ -0,0 +1,52 @@
+const projectList = [
+  {
+    name: "Marcette Website",
+    services: "Development",
+    duration: "8 weeks",
+    year: "2025",
+    preview: "/src/assets/images/Placeholder.png",
+    color: "#2D2D2D",
+  },
+  {
+    name: "Harlow eCommerce",
+    services: "Design & Development",
+    duration: "14 weeks",
+    year: "2025",
+    preview: "/src/assets/images/Placeholder.png",
+    color: "#B6DF70",
+  },
+  {
+    name: "Polygene Webapp",
+    services: "Development",
+    duration: "8 weeks",
+    year: "2025",
+    preview: "/src/assets/images/Placeholder.png",
+    color: "#028E74",
+  },
+  {
+    name: "Daisy’s Landing Page",
+    services: "Design & Development",
+    duration: "10 weeks",
+    year: "2024",
+    preview: "/src/assets/images/Placeholder.png",
+    color: "#FF980B",
+  },
+  {
+    name: "Count Dane Website",
+    services: "Design & Development",
+    duration: "14 weeks",
+    year: "2024",
+    preview: "/src/assets/images/Placeholder.png",
+    color: "#2D2D2D",
+  },
+  {
+    name: "Johnny Website",
+    services: "Design & Development",
+    duration: "12 weeks",
+    year: "2024",
+    preview: "/src/assets/images/Placeholder.png",
+    color: "#03B5FD",
+  },
+];
+
+export { projectList };
